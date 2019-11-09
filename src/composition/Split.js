0ab5eb2c8e3ba1e6dc2d55b32540a404cfbddc11
Bryan Abicht <ixentrik@gmail.com>
2019-11-09 11:19:21 -0800

    First commit for react template

---
+++ src/composition/Split.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Split() {
+  return (
+    <div className="split">
+      Yee thou I now walk through the valley and the shadow of death I shall see
+      no evil.
+    </div>
+  );
+}
+export default Split;
