7ed20641db7f2c44752a0bd03305ce70477e18f6
unknown <ixentrik@gmail.com>
2019-12-12 10:16:12 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
 import React from "react";
-import Split from "./composition/Split";
-import "./App.css";
+import AddItemForm from "./shopping-list/AddItemForm";
+import ShoppingList from "./shopping-list/ShoppingList";
 
-function App() {
-  return (
-    <main className="App">
-      <Split />
-      <Split />
-      <Split />
-      <Split />
-      <Split />
-    </main>
-  );
+class App extends React.Component {
+  state = {
+    shoppingItems: [
+      /* put stub items in here for testing */
+      { name: "apples", checked: false },
+      { name: "oranges", checked: true },
+      { name: "bread", checked: false }
+    ]
+  };
+  handleDeleteItem = item => {
+    const newItems = this.state.shoppingItems.filter(itm => itm !== item);
+    this.setState({
+      shoppingItems: newItems
+    });
+  };
+  handleCheckItem = item => {
+    const newItems = this.state.shoppingItems.map(itm => {
+      if (itm === item) {
+        itm.checked = !itm.checked;
+      }
+      return itm;
+    });
+    this.setState({
+      shoppingItems: newItems
+    });
+  };
+  handleAddItem = itemName => {
+    console.log("handle add item", { itemName });
+  };
+  handleDeleteItem(item) {
+    console.log("handle delete item called", { item });
+  }
+  handleCheckItem(item) {
+    console.log("handle check item called", { item });
+  }
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Shopping List</h1>
+        </header>
+        <main>
+          <section>
+            <AddItemForm onAddItem={this.handleAddItem} />
+          </section>
+          <section>
+            <ShoppingList
+              items={this.state.shoppingItems}
+              onDeleteItem={this.handleDeleteItem}
+              onCheckItem={this.handleCheckItem}
+            />
+          </section>
+        </main>
+      </>
+    );
+  }
 }
-
 export default App;
---
+++ src/shopping-list/AddItemForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class AddItemForm extends React.Component {
+  onSubmitForm = e => {
+    e.preventDefault();
+    // e.target['inputName'] references `<input name='inputName' />`
+    this.props.onAddItem(e.target.itemToAdd.value);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitForm}>
+        <input
+          name="itemToAdd"
+          type="text"
+          placeholder="carrots"
+          aria-label="Shopping list item"
+        />
+        <button type="submit">Add Item</button>
+      </form>
+    );
+  }
+  handleAddItem = itemName => {
+    const newItems = [
+      ...this.state.shoppingItems,
+      { name: itemName, checked: false }
+    ];
+    this.setState({
+      shoppingItems: newItems
+    });
+  };
+}
+export default AddItemForm;
